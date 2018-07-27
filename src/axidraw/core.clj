55b651c73b9f0e8729efd49f466843af70af41d7
skall <blazekm1lan@seznam.cz>
2018-07-27 20:56:59 +0200

    Drawing example.

--- src/axidraw/core.clj
+++ src/axidraw/core.clj
@@ -1,8 +1,53 @@
-(ns axidraw.core)
+(ns axidraw.core
+  (:require [serial.core :as s]
+            [serial.util :as su]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def p "COM4")
+(def port (atom nil))
+(def buffer (atom []))
 
-; (su/list-ports)
+(defn process-buffer []
+  (let [res (->> @buffer (butlast) (clojure.string/join ""))]
+    (println (str "Response: " res)))
+  (reset! buffer []))
+
+(defn receive [x]
+  (let [b (.read x)]
+    (if (= b 10)
+      (process-buffer)
+      (swap! buffer conj (char b)))))
+
+(defn open-port []
+  (reset! port (s/open p))
+  (s/listen! @port receive))
+
+(defn close-port []
+  (s/close @port)
+  (reset! port nil))
+
+(defn send-msg [msg]
+  (when-let [p @port]
+    (println "Sending: " (str msg))
+    (let [b (map (comp byte int) (str msg "\r"))]
+      (s/write @port b))))
+
+(def cord-m (map
+               (fn [[x y]] (clojure.string/replace
+                             (format "G1 X%.2f Y%.2f F10000" x y)
+                             "," "."))
+               [[50.0 0.0]
+                [25.0 30.0]
+                [50.0 60.0]
+                [0.0 60.0]]))
+
+(defn drawM []
+  (open-port)
+  (send-msg "M3 S60")
+  (send-msg "G28")
+  (send-msg "M3 S0")
+  (doall (map send-msg cord-m))
+  (send-msg "M3 S60")
+  (send-msg "G28")
+  (close-port))
+
+(drawM)
